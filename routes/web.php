d930a4cb322281d305c622b2d3a32b4c3e6b4008
Tejas <40323282+tejas9033@users.noreply.github.com>
2025-08-28 10:49:03 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    return view('emails.otp', [
-        'firstName' => 'tejas',
-        'otp' => '123456'
-    ]);
+    return view('welcome');
 });
